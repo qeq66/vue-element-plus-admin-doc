b875a865bc4a3328b173e54b380b2b60adda8d41
kailong321200875 <kailong321200875@users.noreply.github.com>
2022-08-30 03:05:11 +0000

    deploy: 4d4d7b853601014d9283b6472f02175ddfbf098a

---
+++ assets/components_footer.md.73bab232.lean.js
@@ -0,0 +1,1 @@
+import{o as a,c as n,a as s}from"./app.044db66b.js";const t='{"title":"Footer 页脚","description":"","frontmatter":{},"headers":[{"level":2,"title":"用法","slug":"用法"}],"relativePath":"components/footer.md","lastUpdated":1661828693782}',p={},e=s('',5);p.render=function(s,t,p,o,c,l){return a(),n("div",null,[e])};export default p;export{t as __pageData};
